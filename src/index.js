60e5b93b0a5c553a85ec45ea6e44ae67324df2a0
Rishi <suryawanshirishi.nmims@gmail.com>
2020-08-01 18:14:04 +0530

    ReactJS10

--- src/index.js
+++ src/index.js
@@ -1,17 +1,159 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React, { Component } from "react";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+const DemoComponent=React.forwardRef((props,ref)=>{
+
+  function testClick(){
+
+    ref.current.focus();
+
+  }
+
+  return(
+
+    <button onClick={testClick}>Click</button>
+
+  )
+
+});
+
+
+
+class Elevator extends React.Component{
+
+  constructor(props){
+
+    super(props);
+
+    this.elevatorRef=React.createRef();
+
+  }
+
+
+
+  render(){
+
+    return(
+
+      <div>
+
+        <h2>Welcome to Elevator Ordering Screen...</h2>
+
+        <p>
+
+          <label>Elevator Name : <input ref={this.elevatorRef} type="text"></input></label>
+
+        </p>
+
+        <p>
+
+          <label>Elevator Speed : <input type="text"></input></label>
+
+        </p>
+
+        <p>
+
+          <label>Elevator Load : <input type="text"></input></label>
+
+        </p>
+
+        <Summary innerRef={this.elevatorRef}></Summary>
+
+        <DemoComponent ref={this.elevatorRef}></DemoComponent>
+
+      </div>
+
+    );
+
+  }
+
+}
+
+
+
+class Summary extends React.Component{
+
+  constructor(props){
+
+    super(props);
+
+  }
+
+  
+
+  focusInput=()=>{
+
+    this.props.innerRef.current.focus();
+
+  }
+
+  render(){
+
+    return (
+
+      <div>
+
+        <h2>Summary Details...</h2>
+
+        <p onClick={this.focusInput}>
+
+          <label>Elevator Name : <b>Name - 1</b></label>          
+
+        </p>
+
+        <p>
+
+          <label>Elevator Speed : <b>10 m/s</b></label>
+
+        </p>
+
+        <p>
+
+          <label>Elevator Load : <b>550 Kg</b></label>
+
+        </p>
+
+      </div>
+
+    );
+
+  }
+
+}
+
+
+
+function testComponent(){
+
+  
+
+  let testRef=null;
+
+  function handleClick() {
+
+    testRef.focus();
+
+  }
+
+
+
+  return (
+
+    <div>
+
+      <input type="text" ref={e=>testRef=e} />
+
+      <input type="button" value="Focus the text input" onClick={handleClick}/>
+
+    </div>
+
+  );
+
+}
+
+const element=<Elevator></Elevator>
+
+ReactDOM.render(element,document.getElementById("root"));
